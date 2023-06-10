29547e9fedc0e852be052a7f72115a7a03aa6b47
ValentynKrasovskyi <125266163+ValentynKrasovskyi@users.noreply.github.com>
2023-06-10 15:39:16 +0200

    Add files via upload

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,26 @@
+
+import './ContactList.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectVisibleContacts } from 'redux/selectors'; 
+import { deleteContact } from 'redux/contactsSlice';
+
+export const ContactList = () => { 
+  const contacts = useSelector(selectVisibleContacts);
+  const dispatch = useDispatch();
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <p>
+              {name}: {number}
+            </p>
+            <button type="button" onClick={() => dispatch(deleteContact(id))}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
